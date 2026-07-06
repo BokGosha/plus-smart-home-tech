8cd8a28412952c099d14f1a81683a975acf78f69
BokGosha <147842599+BokGosha@users.noreply.github.com>
2026-07-06 23:37:17 +0300

    feat: рефакторинг KafkaClient для улучшения инициализации и управления жизненным циклом

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/kafka/KafkaClientImpl.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.collector.kafka;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.kafka.telemetry.serializer.GeneralAvroSerializer;
+
+import java.util.Properties;
+
+@Component
+public class KafkaClientImpl implements KafkaClient {
+
+    private Producer<String, SpecificRecordBase> producer;
+
+    @PostConstruct
+    public void init() {
+        Properties config = new Properties();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class.getName());
+
+        producer = new KafkaProducer<>(config);
+    }
+
+    @Override
+    public Producer<String, SpecificRecordBase> getProducer() {
+        if (producer == null) {
+            throw new IllegalStateException("Producer is not initialized!");
+        }
+
+        return producer;
+    }
+
+    @PreDestroy
+    public void destroy() {
+        if (producer != null) {
+            producer.flush();
+            producer.close();
+        }
+    }
+}
